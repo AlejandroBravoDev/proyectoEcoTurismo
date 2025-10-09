db2a3a75f8b4475a2cdb0203ad902d32361a27fd
AlejandroBravoDev <alebetan09@gmail.com>
2025-10-08 23:16:01 -0500

    panel Administrativo

--- frontend/React/src/components/panelAdmin/PanelAdmin.jsx
+++ frontend/React/src/components/panelAdmin/PanelAdmin.jsx
@@ -3,33 +3,40 @@ import imgLugares from "../../assets/img1.jpg";
 import imgUsuarios from "../../assets/img6.jpg";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Link } from "react-router-dom";
+import "./tailwind.css";
 
 function PanelAdmin() {
   return (
-    <div className="flex-col justify-center items-center w-screen">
-      <h1 className="text-center text-4xl bold text-amber-300">
+    <div className="flex flex-col justify-center items-center">
+      <h1 className="text-center text-4xl font-medium pt-10">
         Panel Administrativo
       </h1>
-      <div className="cardsContainer">
-        <div className="cardHospedajes">
-          <img src={imgHospedajes} alt="" />
-          <h4>Hospedajes</h4>
+      <div className="flex flex-wrap justify-center items-center pt-20 pb-20 gap-15">
+        <div className="flex items-center justify-evenly flex-col w-100 h-110 shadow-xl  pr-8 pl-8 rounded-md bg-[#f9f9f9]">
+          <img src={imgHospedajes} alt="" className="rounded-lg" />
+          <h4 className="text-xl font-bold">Hospedajes</h4>
           <Link to="/adminHospedajes">
-            <button>Administrar Hospedajes</button>
+            <button className="pl-8 pr-8 pt-2 pb-2 rounded-2xl text-white bg-[#4b8236]">
+              Administrar Hospedajes
+            </button>
           </Link>
         </div>
-        <div className="cardLugares">
-          <img src={imgLugares} alt="" />
-          <h4>Lugares</h4>
+        <div className="flex items-center justify-evenly flex-col w-100 h-110 shadow-xl  pr-8 pl-8 rounded-md bg-[#f9f9f9]">
+          <img src={imgLugares} alt="" className="rounded-lg" />
+          <h4 className="text-xl font-bold">Lugares</h4>
           <Link to="/adminLugares">
-            <button>Administrar Lugares</button>
+            <button className="pl-8 pr-8 pt-2 pb-2 rounded-2xl text-white bg-[#4b8236]">
+              Administrar Lugares
+            </button>
           </Link>
         </div>
-        <div className="cardUsuarios">
-          <img src={imgUsuarios} alt="" />
-          <h4>Usuarios</h4>
+        <div className="flex items-center justify-evenly flex-col w-100 h-110 shadow-xl  pr-8 pl-8 rounded-md bg-[#f9f9f9]">
+          <img src={imgUsuarios} alt="" className="rounded-lg" />
+          <h4 className="text-xl font-bold">Usuarios</h4>
           <Link to="/adminUsuarios">
-            <button>Administrar Usuarios</button>
+            <button className="pl-8 pr-8 pt-2 pb-2 rounded-2xl text-white bg-[#4b8236]">
+              Administrar Usuarios
+            </button>
           </Link>
         </div>
       </div>
