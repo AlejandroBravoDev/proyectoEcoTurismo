278cda60fdd3f8c8e3b6ff041e583d499e6cb48b
Juan Camilo Marin Zapata <marin.jc2005@gmail.com>
2026-02-17 01:33:50 -0500

    Estilos de la seccion de contactos mejorados

--- frontend/React/src/pages/preguntasFrecuentes.jsx
+++ frontend/React/src/pages/preguntasFrecuentes.jsx
@@ -1,13 +1,15 @@
 import FaqEcoturismo from "../components/preguntasFrecuentes/preguntasFrecuentes";
 import Header from "../components/header";
+import Footer from "../components/footer";
 
-function preguntasFrecuentesPage(){
-    return(
-        <>
-            <Header/>
-            <FaqEcoturismo/>
-        </>
-    )
+function preguntasFrecuentesPage() {
+  return (
+    <>
+      <Header />
+      <FaqEcoturismo />
+      <Footer />
+    </>
+  );
 }
 
 export default preguntasFrecuentesPage;
